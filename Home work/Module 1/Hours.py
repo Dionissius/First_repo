16b764d9459d7a5c62317b1fbb28790363c2155a
Denys Borysov <111238634+Dionissius@users.noreply.github.com>
2023-07-08 17:33:27 +0300

    1

---
+++ Home work/Module 1/Hours.py
@@ -0,0 +1,17 @@
+# Программа 
+# Узнать сколько часов-минут-секунд в веддённом количестве секунд
+
+sec_input = int(input('Please enter sec: '))
+
+sec_in_hour = 3600
+sec_in_minutes = 60
+
+hours = int(sec_input / sec_in_hour)
+rest_after_hours = sec_input - (sec_in_hour * hours)
+
+minutes = int(rest_after_hours / sec_in_minutes)
+rest_after_minutes = rest_after_hours - (minutes * sec_in_minutes)
+
+seconds = rest_after_minutes
+
+print(f'In {sec_input} seconds : {hours} hours {minutes} minutes {seconds} seconds')
